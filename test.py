2c6a3ed1fa1d78078b88beb74a8c52231dc72c30
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-05-19 18:22:07 +0200

    implement nx2gt graph conversion

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from sklearn import datasets
-from util import build_graph
+from util import build_graph, nx2gt
 
 iris = datasets.load_iris()
 G = build_graph(iris.data, "manhattan")
 print(len(list(G.nodes())))
 print(len(list(G.edges())))
+
+print(nx2gt(G))
