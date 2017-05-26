f0171095ccc921d0a3301f9668d6d24c2285e4df
Pavlin Poličar <pavlin.g.p@gmail.com>
2017-05-26 09:47:40 +0200

    print latex table in evaluate run

--- evaluate.py
+++ evaluate.py
@@ -26,14 +26,14 @@ class Data:
 
 DATASETS = {
     'iris': Table('iris'),
-    'ecoli': Table('ecoli'),
-    'glass': Table('glass'),
-    'wine': Table('wine'),
+    # 'ecoli': Table('ecoli'),
+    # 'glass': Table('glass'),
+    # 'wine': Table('wine'),
     'zoo': Table('zoo'),
-    'circular': Table('datasets/circular'),
+    # 'circular': Table('datasets/circular'),
     'ina': Table('datasets/ina'),
     'two_moons': Table('datasets/two_moons'),
-    'movements': Data('datasets/movement_libras.data'),
+    # 'movements': Data('datasets/movement_libras.data'),
 }
 
 CLUSTERING_METHODS = {
@@ -58,6 +58,7 @@ def run():
     nmi_scores = defaultdict(dict)
     silhouette_scores = defaultdict(dict)
     ari_scores = defaultdict(dict)
+    n_clusters = defaultdict(dict)
 
     for dataset_name, dataset in DATASETS.items():
         for method_name, method in CLUSTERING_METHODS.items():
@@ -69,10 +70,29 @@ def run():
                 silhouette_score(dataset.X, labels)
             ari_scores[dataset_name][method_name] = \
                 adjusted_rand_score(dataset.Y, labels)
+            n_clusters[dataset_name][method_name] = np.unique(labels).shape[0]
 
     pprint(nmi_scores)
     pprint(silhouette_scores)
     pprint(ari_scores)
+    pprint(n_clusters)
+
+    # Print header
+    print(' & '.join([
+        'Dataset', 'Clustering method', 'Silhouette score', 'NMI score',
+        'ARI score', '$n$ clusters',
+    ]), r'\\ \hline')
+    # TODO Change this when we agree on a better format for the results table
+    for dataset_name in DATASETS:
+        for method_name in CLUSTERING_METHODS:
+            print(' & '.join([
+                method_name,
+                '%.4f' % silhouette_scores[dataset_name][method_name],
+                '%.4f' % nmi_scores[dataset_name][method_name],
+                '%.4f' % ari_scores[dataset_name][method_name],
+                '%d' % n_clusters[dataset_name][method_name],
+            ]), r'\\')
+        print(r'\hline')
 
 
 if __name__ == '__main__':
